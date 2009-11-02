c367b834133730faae5ed452fd46f475a2a92d35
cooldaemon <cooldaemon@gmail.com>
2009-11-02 15:03:48 +0900

    Added a trim function for xml.

--- SimpleHttpClientFilterForXML.m
+++ SimpleHttpClientFilterForXML.m
@@ -3,14 +3,42 @@
 @implementation SimpleHttpClientFilterForXML
 
 //----------------------------------------------------------------------------//
+#pragma mark -- Internal --
+//----------------------------------------------------------------------------//
+
+- (NSData *)trimBlank:(NSData *)data
+{
+    NSInteger length = [data length];
+    NSInteger count = 0;
+    for (
+        const unsigned char *source = [data bytes];
+        count < length;
+        source++, count++
+    ) {
+        if ('<' == *source) {
+            break;
+        }
+    }
+
+    if (0 == count || length - 1 == count) {
+        return data;
+    }
+
+    return data = [data
+        subdataWithRange:NSMakeRange(count, [data length]-count)
+    ];
+}
+
+//----------------------------------------------------------------------------//
 #pragma mark -- APIs --
 //----------------------------------------------------------------------------//
 
 - (id)apply:(NSData *)data
 {
-    NSError *error = nil;
+    data = [self trimBlank:data];
 
-    NSXMLDocument *xmlDocument = [[DDXMLDocument alloc]
+    NSError *error = nil;
+    DDXMLDocument *xmlDocument = [[DDXMLDocument alloc]
         initWithData:data
              options:XML_PARSE_RECOVER
                error:&error
